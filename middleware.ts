c1d37562d9f68a1a62c18943590bda8dfe863695
Anushka404 <anushkatewari2004@gmail.com>
2025-04-06 00:02:16 +0530

    completed middleware

--- middleware.ts
+++ middleware.ts
@@ -3,14 +3,14 @@ import { authMiddleware } from "./middlewares/api/authMiddleware";
 
 
 export const config ={
-    matcher: "/api/:path*"
+    matcher: "/api/:path*",
 };
 
 export default function middleware(request: Request){
 
     const authResult = authMiddleware(request);
-    if(!authResult.isValid){
-        
+    if(!authResult.isValid && request.url.includes("/api/blogs")){
+        return new NextResponse(JSON.stringify({message: "Unauthorized"}), {status: 401});
     }
     return NextResponse.next(); 
 }
